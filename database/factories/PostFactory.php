4ced9de6080abc132d1b085be8853b2dd203f2fa
kerbong <kerbong@gmail.com>
2022-09-23 01:57:59 +0900

    36강 완료

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,9 +4,9 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User; // 아래에서 User::factory()를 실행하기 위해 꼭 import해줘야 합니다
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use App\Models\User; // 아래에서 User::factory()를 실행하기 위해 꼭 import해줘야 합니다
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
@@ -28,12 +28,13 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            'user_id' => User::factory(), // 이렇게만 입력해주면 UserFactory에 정의된 대로 User 데이터가 하나 생성되고, 자동으로 id 값을 잡아서 user_id에 저장해줍니다
-            'title' => $this->faker->sentence,
-            'excerpt' => $this->faker->sentence,
-            'body' => $this->faker->paragraph,
-            'slug' => $this->faker->slug,
+            'user_id' => User::factory(),
             'category_id' => Category::factory(),
+            'title' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
+            'excerpt' => '<p>' . implode('</p><p>', $this->faker->paragraphs(2)) . '</p>',
+            'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(5)) . '</p>',
+
         ];
     }
 }
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -4,7 +4,7 @@
         {{ $trigger }}
     </div>
 
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 overflow-auto max-h-52">
         {{ $slot }}
     </div>
 </div>
