45ec926c49d9298a73a5557a06c0a880c0d509a4
kerbong <kerbong@gmail.com>
2022-09-23 23:03:26 +0900

    38강 완료

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
-
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,27 +17,10 @@ use App\Models\Category;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts->where("title", "like", '%' . request('search') . '%')->orWhere("body", "like", '%' . request('search') . '%');
-    }
+Route::get('/', [ PostController::class, 'index'  ] )->name('home');
 
 
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all(),
-
-    ]);
-})->name('home');
-
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
