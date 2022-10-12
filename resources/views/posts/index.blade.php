46ea838a15494b7fde57cf2862cb79f2cd90097f
kerbong <kerbong@gmail.com>
2022-10-12 15:28:29 +0900

    40강 완료

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    @include ('_posts-header')
+    @include ('posts._header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         {{-- //게시글이 하나라도 있다면 아래 내용 실행 --}}
