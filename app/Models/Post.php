a7249835b33aa768871be2c766549273da628319
kerbong <kerbong@gmail.com>
2022-10-12 11:12:36 +0900

    39강 완료

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,6 +20,14 @@ class Post extends Authenticatable
     {
         $query->when($filters['search'] ?? false, fn ($query, $search) =>
             $query->where("title", "like", '%' . $search . '%')->orWhere("body", "like", '%' . $search . '%'));
+        $query->when($filters['category'] ?? false, fn ($query, $category) =>
+        //     $query->
+        //     whereExists(fn($query)=>
+        //         $query->from('categories')
+        //         ->whereColumn('categories.id', 'posts.category_id')->where('categories.slug', $category))
+        // );
+        $query->whereHas('category', fn($query)=>$query->where('slug', $category))
+    );
 
     }
 
