3464547fdf1367f7bf4faf38a111ba1ee0b81d67
kerbong <kerbong@gmail.com>
2022-10-19 22:23:55 +0900

    44강 완료. paginate 사용법 및 제공되는 옵션 선택방법, withQueryString으로 페이지, 카테고리 중복 쿼리 설정

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,11 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         {{-- //게시글이 하나라도 있다면 아래 내용 실행 --}}
         @if ($posts->count())
+            {{ $posts->links() }}
+
             <x-posts-grid :posts="$posts" />
+
+            {{ $posts->links() }}
             {{-- //그렇지 않다면 포스트가 없다는 내용 반환 --}}
         @else
             <p class="text-center">No posts yet. Plase check back </p>
