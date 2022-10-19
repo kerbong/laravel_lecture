68ab64ab95020d3387ee9d95426700db8d278454
kerbong <kerbong@gmail.com>
2022-10-19 21:58:40 +0900

    43강 완료. model에서 query사용할 때 검색할 범위를 명확히 해주기

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,8 +18,10 @@ class Post extends Authenticatable
 
     public function scopeFilter($query, array $filters)
     {
-        $query->when($filters['search'] ?? false, fn ($query, $search) =>
-            $query->where("title", "like", '%' . $search . '%')->orWhere("body", "like", '%' . $search . '%'));
+        $query->when($filters['search'] ?? false, fn ($query, $search) => $query->where(fn($query) =>
+            $query->where("title", "like", '%' . $search . '%')->orWhere("body", "like", '%' . $search . '%')
+        )
+    );
 
         $query->when($filters['category'] ?? false, fn ($query, $category) =>
             $query->whereHas('category', fn($query)=>$query->where('slug', $category)));
